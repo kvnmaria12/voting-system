ffb383b892a281e859ddf6e4b4ec1cf17803cdaa
Kevin <kevindemaria95@gmail.com>
2023-11-03 12:18:01 +0530

    add key

--- src/app/components/TableData.tsx
+++ src/app/components/TableData.tsx
@@ -36,9 +36,12 @@ function TableData() {
         <Table.HeadCell className='text-xl'>Votes</Table.HeadCell>
       </Table.Head>
       <Table.Body className='divide-y'>
-        {nameList?.map((data) => {
+        {nameList?.map((data, index) => {
           return (
-            <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+            <Table.Row
+              key={index}
+              className='bg-white dark:border-gray-700 dark:bg-gray-800'
+            >
               <Table.Cell className='whitespace-nowrap font-medium text-gray-900 dark:text-white'>
                 {data.userDetails.name}
               </Table.Cell>
